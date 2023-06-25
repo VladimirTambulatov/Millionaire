cbb76e8b4bbea3f919150c3c5e6ec52a5e9877dd
VladimirTambulatov <tambulatovv@yandex.ru>
2023-06-25 21:04:56 +0500

    Questions handler

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-const fs = require('fs');
 
-const text = fs.readFileSync('questions.txt', 'utf-8');
-var lines = text.split('\n'); // разбиваем текст на строки
-var questions = []; // массив для хранения вопросов и ответов
-for (var i = 0; i < lines.length; i += 6){
-    var question = lines[i]
-    var answers = [
-        lines[i + 1], // удаляем номер варианта ответа и очищаем строку
-        lines[i + 2],
-        lines[i + 3],
-        lines[i + 4]
-    ];
-    var correct = parseInt(lines[i + 5])
-    questions.push({ // добавляем объект с вопросом и ответами в массив
-        question: question,
-        answers: answers,
-        correct: correct
-    });
-}
-console.log(questions);
+let questions = [
+    {
+        question: 'Как называется самая маленькая страна в мире по территории?\r',
+        answers: [
+            'a) Ватикан\r',
+            'b) Монако\r',
+            'c) Сан-Марино\r',
+            'd) Лихтенштейн\r'
+        ],
+        correct: 1
+    },
+    {
+        question: 'Как называется столица России?\r',
+        answers: [ 'a) Париж\r', 'b) Лондон\r', 'c) Москва\r', 'd) Берлин\r' ],
+        correct: 2
+    },
+    {
+        question: '',
+        answers: [ undefined, undefined, undefined, undefined ],
+        correct: NaN
+    }
+]
+console.log(questions[0]["question"])
+console.log(questions[0]["answers"])
+console.log(questions[0]["answers"][0])
+
+
 function hint1(){
 }
 function hint2(){
@@ -38,3 +45,35 @@ function timer() {
         }
     },1000)
 }
+function start(){
+    for (let task of questions){
+        if (do_task(task) == false){
+            alert("Вы проиграли!")
+        }
+    }
+}
+
+function do_task({question, answers, correct}) {
+    document.getElementById("question").textContent = question
+    let i = 0
+    for (let label of document.getElementsByClassName("answer")) {
+        label.textContent = answers[i]
+        i++
+    }
+    check()
+}
+function check(correct) {
+    if (document.getElementById("button").pressed) {
+        alert("попа")
+        let inp = document.getElementsByClassName('answer-input');
+        for (let i = 0; i < inp.length; i++) {
+            if (inp[i].type == "radio" && inp[i].checked && inp[i].getAttribute("value") == correct) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
+start()
